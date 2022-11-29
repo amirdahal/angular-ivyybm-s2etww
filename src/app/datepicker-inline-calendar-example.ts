c047cf396d7da0b383af9dc2fcd84e23ac863cd1
amirdahal <68531462+amirdahal@users.noreply.github.com>
2022-11-29 18:26:42 +0530

    1 file modified

--- src/app/datepicker-inline-calendar-example.ts
+++ src/app/datepicker-inline-calendar-example.ts
@@ -1,4 +1,4 @@
-import {Component, Renderer2} from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 /** @title Datepicker inline calendar example */
 @Component({
@@ -7,37 +7,36 @@ import {Component, Renderer2} from '@angular/core';
   styleUrls: ['datepicker-inline-calendar-example.css'],
 })
 export class DatepickerInlineCalendarExample {
-  selected: Date | null;
+  selected = '2022-11-01';
   dates = [
-    { date: "2022-11-01", text: "Hello" },
-    { date: "2020-09-20", text: "Special Day 2" }
+    { date: '2022-11-01', text: 'Hello' },
+    { date: '2020-09-20', text: 'Special Day 2' },
   ];
-  Today  = [{ date: this.dateToString(new Date()), text: "Today" }]
-  constructor(private renderer: Renderer2){}
+  Today = [{ date: this.dateToString(new Date()), text: 'Today' }];
+  constructor(private renderer: Renderer2) {}
 
   displayMonth() {
-    setTimeout(()=>{
-    let elements = document.querySelectorAll(".endDate");
-    console.log("*",elements.length)
-    let x = document.querySelectorAll(".mat-calendar-body-cell");
-    x.forEach(y => {
-      const dateSearch = this.dateToString(
-        new Date(y.getAttribute("aria-label")|| this.Today[0].date)
-      );
-      const data = this.dates.find(f => f.date == dateSearch);
-      const data_today = this.Today.find(f => f.date == dateSearch);
-      if (data) y.setAttribute("aria-label", data.text);
-      if (data_today) y.setAttribute("aria-label", data_today.text);
-    });
-
-    })
-  }
-  streamOpened(event:any) {
     setTimeout(() => {
-      let buttons = document.querySelectorAll("mat-calendar .mat-icon-button");
+      let elements = document.querySelectorAll('.endDate');
+      console.log('*', elements.length);
+      let x = document.querySelectorAll('.mat-calendar-body-cell');
+      x.forEach((y) => {
+        const dateSearch = this.dateToString(
+          new Date(y.getAttribute('aria-label') || this.Today[0].date)
+        );
+        const data = this.dates.find((f) => f.date == dateSearch);
+        const data_today = this.Today.find((f) => f.date == dateSearch);
+        if (data) y.setAttribute('aria-label', data.text);
+        if (data_today) y.setAttribute('aria-label', data_today.text);
+      });
+    });
+  }
+  streamOpened(event: any) {
+    setTimeout(() => {
+      let buttons = document.querySelectorAll('mat-calendar .mat-icon-button');
 
-      buttons.forEach(btn =>
-        this.renderer.listen(btn, "click", () => {
+      buttons.forEach((btn) =>
+        this.renderer.listen(btn, 'click', () => {
           setTimeout(() => {
             //debugger
             this.displayMonth();
@@ -49,32 +48,29 @@ export class DatepickerInlineCalendarExample {
   }
 
   dateClass = (d: Date) => {
-    if (d.getDate()==1)
-      this.displayMonth()
+    if (d.getDate() == 1) this.displayMonth();
     const dateSearch = this.dateToString(d);
-    if (this.Today.find(f => f.date == dateSearch)) {
-      return this.Today.find(f => f.date == dateSearch)
-      ? "todays_class"
-      : "normal";
+    if (this.Today.find((f) => f.date == dateSearch)) {
+      return this.Today.find((f) => f.date == dateSearch)
+        ? 'todays_class'
+        : 'normal';
     } else {
-      return this.dates.find(f => f.date == dateSearch)
-      ? "example-custom-date-class"
-      : "normal";
+      return this.dates.find((f) => f.date == dateSearch)
+        ? 'example-custom-date-class'
+        : 'normal';
     }
-
   };
 
   dateToString(date: any) {
     return (
       date.getFullYear() +
-      "-" +
-      ("0" + (date.getMonth() + 1)).slice(-2) +
-      "-" +
-      ("0" + date.getDate()).slice(-2)
+      '-' +
+      ('0' + (date.getMonth() + 1)).slice(-2) +
+      '-' +
+      ('0' + date.getDate()).slice(-2)
     );
   }
 }
-
 
 /**  Copyright 2022 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
